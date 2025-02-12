81b1847bc0d93ef3ee1625dd9743587a5c0a469c
Alex29754 <145469935+Alex29754@users.noreply.github.com>
2025-02-12 18:36:16 +0200

    +

--- Lab2/5.2.py
+++ Lab2/5.2.py
@@ -1,20 +1,19 @@
-def count_restricted_paths(m, n):
-    right = [0] * (n + 1)
-    up = [0] * (n + 1)
-    right[0] = 1  # Начальная клетка
+def count_paths(m, n):
+    dp = [[0] * (n + 1) for _ in range(m + 1)]
+    dp[0][0] = 1
 
-    for _ in range(m + 1):
-        new_right = [0] * (n + 1)
-        new_up = [0] * (n + 1)
+    for i in range(m + 1):
         for j in range(n + 1):
-            if j == 0:
-                new_right[j] = right[j] + up[j]
-            else:
-                new_right[j] = right[j] + up[j]
-                new_up[j] = new_right[j - 1]
-        right, up = new_right, new_up
+            if i > 0:
+                dp[i][j] += dp[i-1][j]  # Горизонтальный шаг
+            if j > 0:
+                if i == 0:
+                    dp[i][j] += dp[i][j-1]  # Вертикальный шаг, если это первый ряд
+                else:
+                    dp[i][j] += dp[i][j-1]  # Вертикальный шаг, если предыдущий шаг не был вертикальным
 
-    return right[n] + up[n]
+    return dp[m][n]
 
-# Для сетки 18x15 клеток (m=18, n=15)
-print(count_restricted_paths(18, 15))
+m = 18
+n = 15
+print(count_paths(m, n))
